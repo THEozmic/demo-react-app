12a9b0b942b600add42e067e0b0547557ad3d60a
Michael Ozoemena <michaelozoemena2@gmail.com>
2019-09-24 02:07:29 +0100

    Updates

---
+++ precache-manifest.df44ba968044081809dc1e5ac2b613c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6b40a138808069e4441552a2095334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae46618eab2bc8234c02",
+    "url": "/static/css/main.49282035.chunk.css"
+  },
+  {
+    "revision": "37cb47f11fc26a491a48",
+    "url": "/static/js/2.da36bbb8.chunk.js"
+  },
+  {
+    "revision": "ae46618eab2bc8234c02",
+    "url": "/static/js/main.ca7717cb.chunk.js"
+  },
+  {
+    "revision": "929f596e158d02a8c4ab",
+    "url": "/static/js/runtime-main.6ca6cad6.js"
+  }
+]);
